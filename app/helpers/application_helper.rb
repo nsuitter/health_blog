82767265e3592d3f097994f56a85b2f247de48d9
nsuitter <nsuitter@saleforce.com>
2015-05-03 20:48:26 -0700

    Adding Hebrew translations and helper methods for rtl and image loc lecture

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def rtl locale
+    if locale != "he"
+      "<body>".html_safe
+    else
+      "<body dir='rtl'>".html_safe
+    end
+  end
 end
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,10 @@
 module StaticPagesHelper
+  def img_localizer img
+    localized_image = "#{I18n.locale.to_s}/#{img}"
+    if I18n.locale.to_s != 'en' && Rails.application.assets.find_asset(localized_image).nil? == false
+      localized_image
+    else
+      img
+    end
+  end
 end
